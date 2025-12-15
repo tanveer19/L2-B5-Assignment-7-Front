daf70263a2189c1424fd5c5ba0a1c6ed5f1eb5e9
tanveer19 <thjbd19@gmail.com>
2025-12-15 21:53:36 +0600

    updated middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET,
